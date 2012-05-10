1d4dacd3c2a3aece528b2f8d7c1a46a9b1a2e079
Josh Sherman <josh@gravityblvd.com>
2012-05-10 14:58:01 -0400

    Finished up query logic

--- ProjectHoneyPot.php
+++ ProjectHoneyPot.php
@@ -1,9 +1,27 @@
 <?php
 
+/**
+ * Project Honey Pot Class
+ *
+ * @author Josh Sherman
+ * @link   http://github.com/joshtronic/php-projecthoneypot
+ * @link   http://www.projecthoneypot.org/httpbl_configure.php
+ */
 class ProjectHoneyPot
 {
+	/**
+	 * API Key
+	 *
+	 * @access private
+	 * @var    string
+	 */
 	private $api_key = '';
 
+	/**
+	 * Constructor
+	 *
+	 * @param string $api_key PHP API Key (12 characters)
+	 */
 	public function __construct($api_key)
 	{
 		if (preg_match('/^[a-z]{12}$/', $api_key))
@@ -16,8 +34,15 @@ class ProjectHoneyPot
 		}
 	}
 
+	/**
+	 * Query API
+	 *
+	 * @param string $ip_address IPv4 address to check
+	 * @return array results from query
+	 */
 	public function query($ip_address)
 	{
+		// Validate the IP format
 		if (filter_var($ip_address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE))
 		{
 			// Flip tha script
@@ -25,13 +50,42 @@ class ProjectHoneyPot
 			krsort($octets);
 			$reversed_ip = implode('.', $octets);
 
-			$results = dns_get_record($this->api_key . '.' . $reversed_ip . '.dnsbl.httpbl.org');
-			$results = explode('.', $results);
+			// Perform the query
+			$results = dns_get_record($this->api_key . '.' . $reversed_ip . '.dnsbl.httpbl.org', DNS_A);
 
-			if ($results[0] == 127)
+			// Process the results
+			if (isset($results[0]['ip']))
 			{
-				$last_activity = $results[1];
-				$threat_sore   = $results[2];
+				$results = explode('.', $results[0]['ip']);
+
+				if ($results[0] == 127)
+				{
+					$results = array(
+						'last_activity' => $results[1],
+						'threat_score'  => $results[2],
+						'categories'    => $results[3],
+					);
+
+					// Creates an array of categories
+					switch ($results['categories'])
+					{
+						case 0:  $results['categories'] = array('Search Engine');                              break;
+						case 1:  $results['categories'] = array('Suspicious');                                 break;
+						case 2:  $results['categories'] = array('Harvester');                                  break;
+						case 3:  $results['categories'] = array('Suspicious', 'Harvester');                    break;
+						case 4:  $results['categories'] = array('Comment Spammer');                            break;
+						case 5:  $results['categories'] = array('Suspicious', 'Comment Spammer');              break;
+						case 6:  $results['categories'] = array('Harvester', 'Comment Spammer');               break;
+						case 7:  $results['categories'] = array('Suspicious', 'Harvester', 'Comment Spammer'); break;
+						default: $results['categories'] = array('Reserved for Future Use');                    break;
+					}
+
+					return $results;
+				}
+			}
+			else
+			{
+				return array('error' => 'Invalid Response');
 			}
 		}
 
--- example.php
+++ example.php
@@ -4,8 +4,11 @@ require_once 'ProjectHoneyPot.php';
 
 $php = new ProjectHoneyPot('YOUR_KEY');
 
-/*
+// Simulate no record returned
 var_dump($php->query('127.0.0.1'));
+
+// Simulate different types
+/*
 var_dump($php->query('127.1.1.0'));
 var_dump($php->query('127.1.1.1'));
 var_dump($php->query('127.1.1.2'));
@@ -15,6 +18,24 @@ var_dump($php->query('127.1.1.5'));
 var_dump($php->query('127.1.1.6'));
 var_dump($php->query('127.1.1.7'));
 */
+
+// Simulate different threat levels
+/*
+var_dump($php->query('127.1.10.1'));
+var_dump($php->query('127.1.20.1'));
+var_dump($php->query('127.1.40.1'));
+var_dump($php->query('127.1.80.1'));
+*/
+
+// Simulate different number of days
+/*
+var_dump($php->query('127.10.1.1'));
+var_dump($php->query('127.20.1.1'));
+var_dump($php->query('127.40.1.1'));
+var_dump($php->query('127.80.1.1'));
+*/
+
+// Not a simulation, legit comment spammer
 var_dump($php->query('60.169.75.161'));
 
 ?>
