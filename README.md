c76fdf936d043fb1d5855b79c5a4da47f2400b2b
Josh Sherman <josh@gravityblvd.com>
2014-09-17 12:50:54 -0400

    Preparing for a formal release
    Converted tabs to spaces, did some cleanup and updated the composer file.

--- README.md
+++ README.md
@@ -5,20 +5,16 @@
 [![Downloads](http://img.shields.io/packagist/dm/joshtronic/php-projecthoneypot.svg?style=flat)][packagist]
 [![Gittip](http://img.shields.io/gittip/joshtronic.svg?style=flat)][gittip]
 
-[travis]:    http://travis-ci.org/joshtronic/php-projecthoneypot
-[coveralls]: https://coveralls.io/r/joshtronic/php-projecthoneypot
-[packagist]: https://packagist.org/packages/joshtronic/php-projecthoneypot
-[gittip]:    https://www.gittip.com/joshtronic/
-
 PHP Wrapper for Project Honey Pot. Compatible with PHP 5.3+ and HHVM.
 
 ## Installation
 
-The preferred installation method is via `composer`. First add the following to your `composer.json`
+The preferred installation method is via `composer`. First add the following
+to your `composer.json`
 
 ```json
 "require": {
-	"joshtronic/php-projecthoneypot": "dev-master"
+    "joshtronic/php-projecthoneypot": "dev-master"
 }
 ```
 
@@ -42,11 +38,14 @@ print_r($results);
 
 ### Results
 
-Queries will return an array of information about the IP address. The array will contain `last_activity`, `threat_score` and an array of `categories`.
+Queries will return an array of information about the IP address. The array
+will contain `last_activity`, `threat_score` and an array of `categories`.
 
 ### Simulating Results
 
-The folks at Project Honey Pot were kind enough to include a way to simulate results from their http:BL API. You can do so by performing queries against the following IP addresses.
+The folks at Project Honey Pot were kind enough to include a way to simulate
+results from their http:BL API. You can do so by performing queries against
+the following IP addresses.
 
 #### Visitor Types
 
@@ -75,10 +74,21 @@ The folks at Project Honey Pot were kind enough to include a way to simulate res
 
 ## Contributing
 
-Suggestions and bug reports are always welcome, but karma points are earned for pull requests.
+Suggestions and bug reports are always welcome, but karma points are earned
+for pull requests.
 
-Unit tests are required for all contributions. You can run the test suite from the `tests` directory simply by running `phpunit .`
+Unit tests are required for all contributions. You can run the test suite from
+the `tests` directory simply by running `phpunit .`
 
-I also urge you to install a honey pot and donate some MX records to Project Honey Pot. They are doing great work, they provide http:BL as a free service and your donations help make their service ever better!
+I also urge you to install a honey pot and donate some MX records to Project
+Honey Pot. They are doing great work, they provide http:BL as a free service
+and your donations help make their service ever better!
 
-If you’re not already a member, please sign up today with [my referral code](http://www.projecthoneypot.org?rf=123193).
+If you’re not already a member, please sign up today with
+[my referral code][referral].
+
+[coveralls]: https://coveralls.io/r/joshtronic/php-projecthoneypot
+[gittip]:    https://www.gittip.com/joshtronic/
+[packagist]: https://packagist.org/packages/joshtronic/php-projecthoneypot
+[referral]:  http://www.projecthoneypot.org?rf=123193
+[travis]:    http://travis-ci.org/joshtronic/php-projecthoneypot
--- tests/ProjectHoneyPotTest.php
+++ tests/ProjectHoneyPotTest.php
@@ -4,233 +4,232 @@ require_once '../src/ProjectHoneyPot.php';
 
 class ProjectHoneyPotTest extends PHPUnit_Framework_TestCase
 {
-	/**
-	 * @expectedException        Exception
-	 * @expectedExceptionMessage You must specify a valid API key.
-	 */
-	public function testInvalidKey()
-	{
-		new joshtronic\ProjectHoneyPot('foo');
-	}
+    /**
+     * @expectedException        Exception
+     * @expectedExceptionMessage You must specify a valid API key.
+     */
+    public function testInvalidKey()
+    {
+        new joshtronic\ProjectHoneyPot('foo');
+    }
 
-	public function testInvalidIP()
-	{
-		$object = new joshtronic\ProjectHoneyPot('foobarfoobar');
+    public function testInvalidIP()
+    {
+        $object = new joshtronic\ProjectHoneyPot('foobarfoobar');
 
-		$this->assertEquals(
-			array('error' => 'Invalid IP address.'),
-			$object->query('foo')
-		);
-	}
+        $this->assertEquals(
+            array('error' => 'Invalid IP address.'),
+            $object->query('foo')
+        );
+    }
 
-	public function testMissingResults()
-	{
-		$mock = $this->getMock(
-			'joshtronic\ProjectHoneyPot',
-			array('dns_get_record'),
-			array('foobarfoobar')
-		);
+    public function testMissingResults()
+    {
+        $mock = $this->getMock(
+            'joshtronic\ProjectHoneyPot',
+            array('dns_get_record'),
+            array('foobarfoobar')
+        );
 
-		$mock->expects($this->once())
-		     ->method('dns_get_record')
-		     ->will($this->returnValue('foo'));
+        $mock->expects($this->once())
+             ->method('dns_get_record')
+             ->will($this->returnValue('foo'));
 
-		$this->assertFalse($mock->query('1.2.3.4'));
-	}
+        $this->assertFalse($mock->query('1.2.3.4'));
+    }
 
-	public function testCategory0()
-	{
-		$mock = $this->getMock(
-			'joshtronic\ProjectHoneyPot',
-			array('dns_get_record'),
-			array('foobarfoobar')
-		);
+    public function testCategory0()
+    {
+        $mock = $this->getMock(
+            'joshtronic\ProjectHoneyPot',
+            array('dns_get_record'),
+            array('foobarfoobar')
+        );
 
-		$mock->expects($this->once())
-		     ->method('dns_get_record')
-		     ->will($this->returnValue(array(array('ip' => '127.0.0.0'))));
+        $mock->expects($this->once())
+             ->method('dns_get_record')
+             ->will($this->returnValue(array(array('ip' => '127.0.0.0'))));
 
-		$results = $mock->query('1.2.3.4');
+        $results = $mock->query('1.2.3.4');
 
-		$this->assertEquals(array('Search Engine'), $results['categories']);
-	}
+        $this->assertEquals(array('Search Engine'), $results['categories']);
+    }
 
-	public function testCategory1()
-	{
-		$mock = $this->getMock(
-			'joshtronic\ProjectHoneyPot',
-			array('dns_get_record'),
-			array('foobarfoobar')
-		);
+    public function testCategory1()
+    {
+        $mock = $this->getMock(
+            'joshtronic\ProjectHoneyPot',
+            array('dns_get_record'),
+            array('foobarfoobar')
+        );
 
-		$mock->expects($this->once())
-		     ->method('dns_get_record')
-		     ->will($this->returnValue(array(array('ip' => '127.0.0.1'))));
+        $mock->expects($this->once())
+             ->method('dns_get_record')
+             ->will($this->returnValue(array(array('ip' => '127.0.0.1'))));
 
-		$results = $mock->query('1.2.3.4');
+        $results = $mock->query('1.2.3.4');
 
-		$this->assertEquals(array('Suspicious'), $results['categories']);
-	}
+        $this->assertEquals(array('Suspicious'), $results['categories']);
+    }
 
-	public function testCategory2()
-	{
-		$mock = $this->getMock(
-			'joshtronic\ProjectHoneyPot',
-			array('dns_get_record'),
-			array('foobarfoobar')
-		);
+    public function testCategory2()
+    {
+        $mock = $this->getMock(
+            'joshtronic\ProjectHoneyPot',
+            array('dns_get_record'),
+            array('foobarfoobar')
+        );
 
-		$mock->expects($this->once())
-		     ->method('dns_get_record')
-		     ->will($this->returnValue(array(array('ip' => '127.0.0.2'))));
+        $mock->expects($this->once())
+             ->method('dns_get_record')
+             ->will($this->returnValue(array(array('ip' => '127.0.0.2'))));
 
-		$results = $mock->query('1.2.3.4');
+        $results = $mock->query('1.2.3.4');
 
-		$this->assertEquals(array('Harvester'), $results['categories']);
-	}
+        $this->assertEquals(array('Harvester'), $results['categories']);
+    }
 
-	public function testCategory3()
-	{
-		$mock = $this->getMock(
-			'joshtronic\ProjectHoneyPot',
-			array('dns_get_record'),
-			array('foobarfoobar')
-		);
+    public function testCategory3()
+    {
+        $mock = $this->getMock(
+            'joshtronic\ProjectHoneyPot',
+            array('dns_get_record'),
+            array('foobarfoobar')
+        );
 
-		$mock->expects($this->once())
-		     ->method('dns_get_record')
-		     ->will($this->returnValue(array(array('ip' => '127.0.0.3'))));
+        $mock->expects($this->once())
+             ->method('dns_get_record')
+             ->will($this->returnValue(array(array('ip' => '127.0.0.3'))));
 
-		$results = $mock->query('1.2.3.4');
+        $results = $mock->query('1.2.3.4');
 
-		$this->assertEquals(
-			array('Suspicious', 'Harvester'),
-			$results['categories']
-		);
-	}
+        $this->assertEquals(
+            array('Suspicious', 'Harvester'),
+            $results['categories']
+        );
+    }
 
-	public function testCategory4()
-	{
-		$mock = $this->getMock(
-			'joshtronic\ProjectHoneyPot',
-			array('dns_get_record'),
-			array('foobarfoobar')
-		);
+    public function testCategory4()
+    {
+        $mock = $this->getMock(
+            'joshtronic\ProjectHoneyPot',
+            array('dns_get_record'),
+            array('foobarfoobar')
+        );
 
-		$mock->expects($this->once())
-		     ->method('dns_get_record')
-		     ->will($this->returnValue(array(array('ip' => '127.0.0.4'))));
+        $mock->expects($this->once())
+             ->method('dns_get_record')
+             ->will($this->returnValue(array(array('ip' => '127.0.0.4'))));
 
-		$results = $mock->query('1.2.3.4');
+        $results = $mock->query('1.2.3.4');
 
-		$this->assertEquals(
-			array('Comment Spammer'),
-			$results['categories']
-		);
-	}
+        $this->assertEquals(
+            array('Comment Spammer'),
+            $results['categories']
+        );
+    }
 
-	public function testCategory5()
-	{
-		$mock = $this->getMock(
-			'joshtronic\ProjectHoneyPot',
-			array('dns_get_record'),
-			array('foobarfoobar')
-		);
+    public function testCategory5()
+    {
+        $mock = $this->getMock(
+            'joshtronic\ProjectHoneyPot',
+            array('dns_get_record'),
+            array('foobarfoobar')
+        );
 
-		$mock->expects($this->once())
-		     ->method('dns_get_record')
-		     ->will($this->returnValue(array(array('ip' => '127.0.0.5'))));
+        $mock->expects($this->once())
+             ->method('dns_get_record')
+             ->will($this->returnValue(array(array('ip' => '127.0.0.5'))));
 
-		$results = $mock->query('1.2.3.4');
+        $results = $mock->query('1.2.3.4');
 
-		$this->assertEquals(
-			array('Suspicious', 'Comment Spammer'),
-			$results['categories']
-		);
-	}
+        $this->assertEquals(
+            array('Suspicious', 'Comment Spammer'),
+            $results['categories']
+        );
+    }
 
-	public function testCategory6()
-	{
-		$mock = $this->getMock(
-			'joshtronic\ProjectHoneyPot',
-			array('dns_get_record'),
-			array('foobarfoobar')
-		);
+    public function testCategory6()
+    {
+        $mock = $this->getMock(
+            'joshtronic\ProjectHoneyPot',
+            array('dns_get_record'),
+            array('foobarfoobar')
+        );
 
-		$mock->expects($this->once())
-		     ->method('dns_get_record')
-		     ->will($this->returnValue(array(array('ip' => '127.0.0.6'))));
+        $mock->expects($this->once())
+             ->method('dns_get_record')
+             ->will($this->returnValue(array(array('ip' => '127.0.0.6'))));
 
-		$results = $mock->query('1.2.3.4');
+        $results = $mock->query('1.2.3.4');
 
-		$this->assertEquals(
-			array('Harvester', 'Comment Spammer'),
-			$results['categories']
-		);
-	}
+        $this->assertEquals(
+            array('Harvester', 'Comment Spammer'),
+            $results['categories']
+        );
+    }
 
-	public function testCategory7()
-	{
-		$mock = $this->getMock(
-			'joshtronic\ProjectHoneyPot',
-			array('dns_get_record'),
-			array('foobarfoobar')
-		);
+    public function testCategory7()
+    {
+        $mock = $this->getMock(
+            'joshtronic\ProjectHoneyPot',
+            array('dns_get_record'),
+            array('foobarfoobar')
+        );
 
-		$mock->expects($this->once())
-		     ->method('dns_get_record')
-		     ->will($this->returnValue(array(array('ip' => '127.0.0.7'))));
+        $mock->expects($this->once())
+             ->method('dns_get_record')
+             ->will($this->returnValue(array(array('ip' => '127.0.0.7'))));
 
-		$results = $mock->query('1.2.3.4');
+        $results = $mock->query('1.2.3.4');
 
-		$this->assertEquals(
-			array('Suspicious', 'Harvester', 'Comment Spammer'),
-			$results['categories']
-		);
-	}
+        $this->assertEquals(
+            array('Suspicious', 'Harvester', 'Comment Spammer'),
+            $results['categories']
+        );
+    }
 
-	public function testCategoryDefault()
-	{
-		$mock = $this->getMock(
-			'joshtronic\ProjectHoneyPot',
-			array('dns_get_record'),
-			array('foobarfoobar')
-		);
+    public function testCategoryDefault()
+    {
+        $mock = $this->getMock(
+            'joshtronic\ProjectHoneyPot',
+            array('dns_get_record'),
+            array('foobarfoobar')
+        );
 
-		$mock->expects($this->once())
-		     ->method('dns_get_record')
-		     ->will($this->returnValue(array(array('ip' => '127.0.0.255'))));
+        $mock->expects($this->once())
+             ->method('dns_get_record')
+             ->will($this->returnValue(array(array('ip' => '127.0.0.255'))));
 
-		$results = $mock->query('1.2.3.4');
+        $results = $mock->query('1.2.3.4');
 
-		$this->assertEquals(
-			array('Reserved for Future Use'),
-			$results['categories']
-		);
-	}
+        $this->assertEquals(
+            array('Reserved for Future Use'),
+            $results['categories']
+        );
+    }
 
-	public function testWithout127()
-	{
-		$mock = $this->getMock(
-			'joshtronic\ProjectHoneyPot',
-			array('dns_get_record'),
-			array('foobarfoobar')
-		);
+    public function testWithout127()
+    {
+        $mock = $this->getMock(
+            'joshtronic\ProjectHoneyPot',
+            array('dns_get_record'),
+            array('foobarfoobar')
+        );
 
-		$mock->expects($this->once())
-		     ->method('dns_get_record')
-		     ->will($this->returnValue(array(array('ip' => '1.0.0.0'))));
+        $mock->expects($this->once())
+             ->method('dns_get_record')
+             ->will($this->returnValue(array(array('ip' => '1.0.0.0'))));
 
-		$this->assertFalse($mock->query('1.2.3.4'));
-	}
+        $this->assertFalse($mock->query('1.2.3.4'));
+    }
 
-	// Doesn't serve much purpose aside from helping achieve 100% coverage
-	public function testDnsGetRecord()
-	{
-		$object = new joshtronic\ProjectHoneyPot('foobarfoobar');
+    // Doesn't serve much purpose aside from helping achieve 100% coverage
+    public function testDnsGetRecord()
+    {
+        $object = new joshtronic\ProjectHoneyPot('foobarfoobar');
 
-		$object->dns_get_record('1.2.3.4');
-	}
+        $object->dns_get_record('1.2.3.4');
+    }
 }
 
-?>
